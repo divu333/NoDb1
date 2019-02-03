e2e880ecbf22fb0e881566d902110137d55b9b91
divya <divu333@gmail.com>
2019-02-03 13:36:41 -0700

    s

--- src/App.js
+++ src/App.js
@@ -1,144 +1,77 @@
-import React, { Component } from 'react';
-<<<<<<< HEAD
-import ViewRecipe from './components/ViewRecipe/ViewRecipe';
-import axios from 'axios';
-import form from './components/form/form.css';
-=======
-import './App.css';
-import { BrowserRouter as Router, Link} from 'react-router-dom';
-import router from './router';
->>>>>>> 9e94f940372d4b7ba07bed39503f6ce11475f2ac
+import React, { Component } from "react";
+import ViewRecipe from "./components/ViewRecipe/ViewRecipe";
+import axios from "axios";
+import form from "./components/form/form.css";
 
 class App extends Component {
-  
-
-  
-<<<<<<< HEAD
-  
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-        newRecipe : '',
-        newIngredients: '',
-        currentId: null,
+      newRecipe: "",
+      newIngredients: "",
+      currentId: null
+    };
+  }
 
-    }
-}
+  handleChange(e) {
+    this.setState({ newRecipe: e.target.value });
+    this.setState({ newIngredients: e.target.value });
+  }
 
-
-
-   
-handleChange(e) {
-    this.setState({ newRecipe:e.target.value})
-    this.setState({ newIngredients:e.target.value})
-}
-
-  
-
-
-handleSubmit () {
-    
+  handleSubmit() {
     const newRecipeBox = {
-        recipeName: this.state.newRecipe,
-        ingredients:this.state.newIngredients
-    }
+      recipeName: this.state.newRecipe,
+      ingredients: this.state.newIngredients
+    };
 
-axios.post('/api/AddRecipeBox',newRecipeBox)
-    }
-      
-    
-handleCancel (){
+    axios.post("/api/AddRecipeBox", newRecipeBox);
+  }
 
-    const newRecipeBox = {
-        recipeName: "",
-        ingredients:""
-    }
-}
-
-  
-    render() {
-
-    return ( 
-
+  render() {
+    return (
       <div>
         <h1 className="h1-recipe">Enter your recipes here</h1>
         <div className="container">
-        <form >
-              
-                <input
-                type = "text"
-                        placeholder =" Recipe Name"
-                        name="recipeName"
-                        onChange={(e) =>{ this.setState({newRecipe:e.target.value })}} 
-                    />
-                    
-         <br/>
-                   
-                    
+          <form>
+            <input
+              type="text"
+              placeholder=" Recipe Name"
+              name="recipeName"
+              onChange={e => {
+                this.setState({ newRecipe: e.target.value });
+              }}
+            />
 
-                    <textarea 
-                    placeholder ="Ingredients  "
-                    type="textarea"
-                     name="ingredients" 
-                      onChange={(e) =>{ this.setState({newIngredients:e.target.value })}} 
-                      />
-
-               <br/>
-
-               <button className ="formBtn"onClick ={e => this.handleSubmit(e)} > Add </button>
-              
-               
-               <button className ="formBtn" onClick ={e => this.handleCancel} > Cancel  </button>               
-        </form>
-
-       {/* <ViewRecipe/> */}
-       <ViewRecipe recipeBox = {this.state.recipeBox} />
-        </div>
-        </div>
-    );
-
-    }
-}
- 
-
-export default App;
-
-=======
- 
-  render(){ 
-     return ( 
-       <Router>
-     <div className = "App">
-     <div className="ListWrapper">
-     <div className="logo"> </div>
-    
-       <nav>
-          <ul className="nav-bar">
-          <li className="navrBar-item"> <Link to='/CreateRecipe' style={{ textDecoration: 'none' }} >Add New recipe</Link></li>
-          <br/>
-            <li className="navrBar-item"> <Link to='/UpdateRecipe' style={{ textDecoration: 'none' }} >Update Recipes</Link></li>
-          <br/>
-            <li className="navrBar-item"> <Link to='/ViewRecipe' style={{ textDecoration: 'none' }} >Show Recipes</Link></li>
             <br />
-            <li className="navrBar-item"><Link to='/DeleteRecipe' style={{ textDecoration: 'none' }}>Delete Recipe</Link></li> 
-          </ul>
-        </nav>
 
-        
-        {router}
-     </div>
-     
+            <textarea
+              placeholder="Ingredients  "
+              type="textarea"
+              name="ingredients"
+              onChange={e => {
+                this.setState({ newIngredients: e.target.value });
+              }}
+            />
+
+            <br />
+
+            <button className="formBtn" onClick={e => this.handleSubmit(e)}>
+              {" "}
+              Add{" "}
+            </button>
+
+            <button className="formBtn" onClick={e => this.handleCancel}>
+              {" "}
+              Cancel{" "}
+            </button>
+          </form>
+
+          {/* <ViewRecipe/> */}
+          <ViewRecipe />
+        </div>
       </div>
-
-      </Router>
-
-        );
-      }
-
-    }
-  
+    );
+  }
+}
 
 export default App;
-
-
->>>>>>> 9e94f940372d4b7ba07bed39503f6ce11475f2ac
--- src/components/ViewRecipe/ViewRecipe.js
+++ src/components/ViewRecipe/ViewRecipe.js
@@ -1,88 +1,59 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import viewRecipe from '../ViewRecipe/viewRecipe.css'
+import React, { Component } from "react";
+import axios from "axios";
+import "../ViewRecipe/viewRecipe.css";
 
-class  ViewRecipe extends Component  {
-
-<<<<<<< HEAD
-  constructor(){
-    super()
+class ViewRecipe extends Component {
+  constructor() {
+    super();
     this.state = {
-      recipeBox :[]
-    }
+      recipeBox: []
+    };
+    this.getRecipeList = this.getRecipeList.bind(this);
+
+    this.deleteRecipe = this.deleteRecipe.bind(this);
   }
 
-  componentDidMount () {
-
-    axios.get('/api/recipeBox').then ((res) => {
-       console.log(res.data)
-       this.setState ({
-        recipeBox : res.data
-       })
-    })
-  
+  getRecipeList() {
+    axios.get("/api/recipeBox").then(res => {
+      console.log(res.data);
+      this.setState({
+        recipeBox: res.data
+      });
+    });
   }
-  render(){
-    return ( 
-   
+
+  componentDidMount() {
+    this.getRecipeList();
+  }
+
+  deleteRecipe() {
+    console.log("button works");
+    let promise = axios.delete("/api/updateRecipeBox/:id");
+    promise.then(() => {
+      this.getRecipeList();
+    });
+  }
+
+  render() {
+    return (
       <div className="viewRecipe">
-           <h1> Recipe List</h1>
-           <ul> 
-           { this.state.recipeBox.map (recipeBox =>
-             <li key={recipeBox.id}> {recipeBox.recipeName}<br/> <br/> 
-             {recipeBox.ingredients} <br/>  <br/></li>
-     
-             )}
-     
-           </ul>
+        <h1> Recipe List</h1>
+        <ul>
+          {this.state.recipeBox.map(recipeBox => (
+            <li key={recipeBox.id}>
+              {" "}
+              {recipeBox.recipeName}
+              <button className="delete_button" onClick={this.deleteRecipe}>
+                X
+              </button>
+              <br /> <br />
+              {recipeBox.ingredients} <br /> <br />
+            </li>
+          ))}
+        </ul>
       </div>
-          );
-
+    );
   }
 }
 
-
-
-
-=======
-constructor(){
-  super()
-  this.state = {
-    recipeBox :[]
-  }
-}
-
-
-componentDidMount () {
-
-  axios.get('/api/recipeBox').then ((res) => {
-     console.log(res.data)
-     this.setState ({
-      recipeBox : res.data
-     })
-  })
-
-}
-render(){ 
-
-
-    
-  return ( 
-   
- <div className="viewRecipe">
-      <h1> Recipe List</h1>
-      <ul> 
-      { this.state.recipeBox.map (recipeBox =>
-        <li key={recipeBox.id}> {recipeBox.recipeName}<br/> <br/> 
-        {recipeBox.ingredients} <br/>  <br/></li>
-
-        )}
-
-      </ul>
- </div>
-     );
-   }
-
- }
->>>>>>> 9e94f940372d4b7ba07bed39503f6ce11475f2ac
 export default ViewRecipe;
